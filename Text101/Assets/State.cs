ee705ec95a697de7a76decb26e67b74bb54bcb5a
Spyros Argalias <sargalias@gmail.com>
2019-04-26 02:01:52 +0100

    First commit.

---
+++ Text101/Assets/State.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "State")]
+public class State : ScriptableObject {
+    // Start is called before the first frame update
+    [SerializeField]
+    [TextArea(10, 14)]
+    string storyText;
+
+    string getStoryText() {
+        return storyText;
+    }
+}
