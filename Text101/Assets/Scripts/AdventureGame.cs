adb8d8305076b97ae7ffcb3ccf293ce5c73f57dd
Spyros Argalias <sargalias@gmail.com>
2019-04-26 05:44:38 +0100

    Organise files.

---
+++ Text101/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+    [SerializeField]
+    Text textComponent;
+
+    [SerializeField]
+    State startingState;
+
+    private State state;
+
+    // Start is called before the first frame update
+    void Start() {
+        state = startingState;
+        DisplayState();
+    }
+
+    void DisplayState() {
+        textComponent.text = state.GetStoryText();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        UpdateState();
+        DisplayState();
+    }
+
+    private void UpdateState() {
+        State[] nextStates = state.GetNextStates();
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            state = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            state = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            state = nextStates[2];
+        }
+    }
+}
---
+++ Text101/Assets/Scripts/State.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "State")]
+public class State : ScriptableObject {
+    // Start is called before the first frame update
+    [SerializeField]
+    [TextArea(10, 14)]
+    string storyText;
+
+    [SerializeField]
+    State[] nextStates;
+
+    public string GetStoryText() {
+        return storyText;
+    }
+
+    public State[] GetNextStates() {
+        return nextStates;
+    }
+}
